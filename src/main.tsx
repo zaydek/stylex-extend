242ea5dc693f0d290c2efbe7822a17185e23ff4e
Zaydek MG <zaydekdotcom@gmail.com>
2024-05-21 02:51:19 -0700

    First commit

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+interface ButtonProps {
+  color: string;
+  onClick: () => void;
+}
+
+function Button(props: React.PropsWithChildren<ButtonProps>) {
+  return (
+    <div onClick={props.onClick} stylex={{ color: props.color, fontSize: "15px" }}>
+      {props.children}
+    </div>
+  );
+}
+
+function App() {
+  const [color, setColor] = useState("red");
+
+  return (
+    <div>
+      <Button color={color} onClick={() => setColor((pre) => (pre === "red" ? "blue" : "red"))}>
+        Action
+      </Button>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.querySelector("#app")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { stylex } from "vite-plugin-stylex-dev";
+
+export default defineConfig({
+  plugins: [react(), stylex({ enableStylexExtend: true })],
+});
